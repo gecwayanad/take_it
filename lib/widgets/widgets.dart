06faf24348d107aafdc0bc2614e644e36116ba98
amithlal200047 <amithlalmv333@gmail.com>
2021-04-27 00:38:41 +0530

    the page showmedicinpage is added

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MedicineEdit extends StatefulWidget {
+  String medicinename;
+
+  MedicineEdit({this.medicinename}); //constructor
+  @override
+  _MedicineEditState createState() => _MedicineEditState();
+}
+
+class _MedicineEditState extends State<MedicineEdit> {
+  bool check = true;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.red[300],
+                  border: Border.all(color: Colors.red[300]),
+                  borderRadius: BorderRadius.circular(15)),
+              height: 80,
+              width: 350,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  TextButton(
+                      //text button name to go back to its edit screen
+                      onPressed: () {},
+                      child: Text(
+                        widget.medicinename,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      )),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  Checkbox(
+                    //checkbox
+                    value: check,
+                    activeColor: Colors.red[200],
+                    onChanged: (bool value) {
+                      setState(() {
+                        check = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ));
+  }
+}
